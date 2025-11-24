0590bca11f363508d937714b8e3f03e24f444bcc
Vinicius-1300 <VMaximus015@gmail.com>
2025-11-24 18:25:32 -0300

    Criação do botão, por enquanto sem função, responsável pela criação dos hábitos/tarefas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,13 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity} from 'react-native';
+import { button_create_task } from '../styles/styles_components.js'
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View>
+    <View style={{ flex: 1}}>
       <Text>Welcome to the Home Screen!</Text>
+      <TouchableOpacity style={button_create_task}>
+      <Text style={{color: '#FFFFFF'}}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
---
+++ src/styles/styles_components.js
@@ -0,0 +1,12 @@
+export const button_create_task = {
+    backgroundColor: 'blue',
+    color: '#FFFFFF',
+    position: 'absolute',
+    borderRadius: 20,
+    bottom: 20,
+    right: 20,
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center'
+}
