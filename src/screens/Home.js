00c26c9b568e2c55186d9bd3188e569130c678e4
Friederich <siyfred1n@gmail.com>
2025-11-26 02:58:35 -0300

    fix: FAB Button atualizado para seguir os padroes do Google Material 3 Design

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,22 +1,47 @@
-import { View, Text, TouchableOpacity} from 'react-native';
+import { useState } from 'react';
+import { View, Text } from 'react-native';
+import { AnimatedFAB } from 'react-native-paper';
 import { Ionicons } from '@expo/vector-icons';
-import { button_create_habit } from '../styles/styles_components.js'
+import { fab } from '../styles/styles_components.js'
 
 export default function HomeScreen({ navigation }) {
+  const [isExtended, setIsExtended] = useState(true);
+
+  function onScroll({ nativeEvent }) {
+    const currentScrollPosition = Math.floor(nativeEvent.contentOffset.y) ?? 0;
+    if (currentScrollPosition > 30) {
+      setIsExtended(false);
+    } 
+    else if (currentScrollPosition <= 0) {
+      setIsExtended(true);
+    }
+  }
 
   function handleCreateHabit() {
     
   }
 
   return (
-    <View style={{ flex: 1}}>
+    <View style={{ flex: 1 }}>
       <Text>Welcome to the Home Screen!</Text>
-      <TouchableOpacity
-        style={button_create_habit}
-        onPress={handleCreateHabit}
-      >
-      <Ionicons name="add" size={30} color="#FFF" />
-      </TouchableOpacity>
+      <AnimatedFAB
+        label="Novo Hábito "
+        icon={({ size, color }) => (
+          <Ionicons name="add" size={size} color={color}/>
+        )}
+        labelStyle={{
+          marginTop: -1, 
+          fontSize: 16,
+          fontWeight: '600',
+        }}
+        extended={isExtended} 
+        onPress={() => console.log('Clicou')}
+        visible={true} 
+        animateFrom={'right'}
+        iconMode={'dynamic'}
+        style={fab}
+        color="#FFF"
+      />
     </View>
   );
 }
--- src/styles/styles_components.js
+++ src/styles/styles_components.js
@@ -1,18 +1,18 @@
 import { colors_white } from './theme.js';
 
-export const button_create_habit = {
+export const fab = {
   backgroundColor: colors_white.primary,
   position: 'absolute',
-  borderRadius: 30,
+  borderRadius: 16,
   bottom: 20,
   right: 20,
-  width: 60,
-  height: 60,
-  justifyContent: 'center',
-  alignItems: 'center',
-  elevation: 5,
-  shadowColor: '#000',
-  shadowOffset: { width: 0, height: 2 },
-  shadowOpacity: 0.25,
-  shadowRadius: 3.84
+  shadowColor: "#000",
+  shadowOffset: {
+    width: 0,
+    height: 4,
+  },
+  shadowOpacity: 0.30,
+  shadowRadius: 4.65,
+  elevation: 4,
+  // paper gerencia outras propriedades internamente
 }
